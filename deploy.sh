e4584762fd630f0ad48ca9ccdafcd119f61eec7f
Yoga-Saputra <yogasaputran@gmail.com>
2022-06-24 10:59:00 +0700

    add bash script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-cd /var/www/Qasir-App
-git pull origin master
-composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
-php artisan config:cache
-php artisan config:clear
-ls -lah
-sudo chown -R www-data.www-data /var/www/Qasir-App/storage
-chown -R www-data.www-data /var/www/Qasir-App/bootstrap/cache
