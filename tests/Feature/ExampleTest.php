ace46ce4ec1280f7e9621100f4d604e805a924a4
Yoga-Saputra <yogasaputran@gmail.com>
2022-06-24 10:04:44 +0700

    add bash script

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        // we dont need this
         // $response = $this->get('/');
 
         // $response->assertStatus(200);
